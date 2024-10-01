82e1481b1f479aecffa63faf8d4c76984caeaa5b
Jewel Hasan <jewel.hasan@cg-bd.com>
2024-10-01 16:52:39 +0600

    basic design

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,40 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/Root.jsx';
+import Home from './components/Home.jsx'
+import LogIn from './components/LogIn.jsx';
+import Register from './components/Register.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children:[
+      {
+        path: "/",
+        element: <Home></Home> 
+      },
+      {
+        path: "/login",
+        element: <LogIn></LogIn>, 
+      },
+      {
+        path: "/register",
+        element: <Register></Register>, 
+      },
+      
+    ]
+  },
+
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <RouterProvider router={router} />
   </StrictMode>,
 )
