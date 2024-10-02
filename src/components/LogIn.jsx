80ac010586e5332a98228e08e67f2530784b135a
Jewel Hasan <jewel.hasan@cg-bd.com>
2024-10-02 16:57:01 +0600

    login finished

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,50 +1,70 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { nameContext } from './ContextAP';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import app from './firebase.config';
 
 const LogIn = () => {
+   // Context API 
   const AuthInfo = useContext(nameContext);
-  console.log(AuthInfo)
 
+
+// Log In by email and password
+  const [userLogIn, setUserLogIn] = useState(null);
+  const [errorLogIn, setErrorLogIn] = useState(null);
+
+  const handleLogIn = e =>{
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log('login', email, password)
+    
+    const auth = getAuth(app)
+    signInWithEmailAndPassword(auth, email, password)
+    .then(result => {
+      const logId = result.user;
+      setUserLogIn(logId)
+      console.log(logId);
+    })
+    .catch(error=>{
+      const errorAll = error.message;
+      setErrorLogIn(errorAll);
+    })
+  }
     return (
         <div>
     <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
+  <div className="hero-content flex-col ">
     <div className="text-center lg:text-left">
-      <div>
-        <h1>Name of User is :{AuthInfo.name}</h1>
-        <h1>Name of User is :{AuthInfo.age}</h1>
-      </div>
+     
       <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
+      
     </div>
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <form className="card-body">
+      <form className="card-body" onSubmit={handleLogIn}>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="email" name='email' placeholder="email" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="password" name='password' placeholder="password" className="input input-bordered" required />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button className="btn btn-primary" >Login</button>
         </div>
       </form>
+      </div>
     </div>
   </div>
 </div>
-        </div>
+       
     );
 };
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,36 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { nameContext } from './ContextAP';
+import app from './firebase.config';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
 const Register = () => {
+    // Context API 
     const AuthInfo = useContext(nameContext);
-    console.log(AuthInfo)
+
+    // Create new account by email and password
+    const [errorInfo, setErrorInfo] = useState(null);
+
+    const handleRegister= e =>{
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log('Register submitted', name, email, password)
+
+        const auth = getAuth(app)
+        createUserWithEmailAndPassword(auth, email, password)
+        .then( result => {
+            const userId = result.user;
+            console.log(userId);
+        })
+        .catch(error =>{
+            const errorAll = error.message;
+            setErrorInfo(errorAll);
+            console.log('error', error.message)
+
+        })
+    }
+    
     return (
         <div>
          <div className="hero bg-base-200 min-h-screen">
@@ -14,24 +41,27 @@ const Register = () => {
             Age: {AuthInfo.age}
         </div>
       <h1 className="text-5xl font-bold">Register your profile</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
+     
     </div>
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <form className="card-body">
+      <form className="card-body" onSubmit={handleRegister}>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input type="name" name='name' placeholder="Name" className="input input-bordered" required />
+        </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="email" name='email' placeholder="email" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="password" name='password' placeholder="password" className="input input-bordered" required />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
@@ -39,7 +69,13 @@ const Register = () => {
         <div className="form-control mt-6">
           <button className="btn btn-primary">Login</button>
         </div>
+       
       </form>
+      <div className='text-red-400 font-bold p-3 m-3'>
+      {
+        errorInfo
+      }
+      </div>
     </div>
   </div>
 </div>
